f6591aafabba1174bc16f8ba18c000a4087a3159
Ankit <ankit.prajapati@mindinventory.com>
2026-05-06 14:37:18 +0530

    fix(backend): removed api route for two

--- backend/index.js
+++ backend/index.js
@@ -39,9 +39,7 @@ app.use("/apiOne", (req, res) => {
     res.status(200).send('apiOne is working!!!');
 })
 
-app.use("/apiTwo", (req, res) => {
-    res.status(200).send('apiTwo is working!!!');
-})
+
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
